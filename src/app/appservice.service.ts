ffc7fa5db0e6b43c30af253597453721c0eb6994
Adebayo Oyebamiji <adebayo.oyebamiji@ibedc.com>
2022-12-29 14:32:12 +0100

    Evaluation and precomissioning form done

--- src/app/appservice.service.ts
+++ src/app/appservice.service.ts
@@ -599,6 +599,91 @@ addNewUser( formvalue:any
         formData.append('eval_voltage_level', form.eval_voltage_level);
         formData.append('eval_estimated_load', form.eval_estimated_load);
         formData.append('te_is_connection_approved', form.eval_site_visit_date);
+        
+        formData.append('eval_site_visit_date', form.eval_site_visit_date);
+        formData.append('eval_new4upgrade', form.eval_new4upgrade);
+        formData.append('eval_conworkdone', form.eval_conworkdone);
+        formData.append('eval_dtsubname', form.eval_dtsubname);
+        formData.append('eval_region', form.eval_region);
+        formData.append('eval_bhub', form.eval_bhub);
+        
+        formData.append('eval_comentoncon', form.eval_comentoncon);
+        formData.append('eval_fdrname', form.eval_fdrname);
+        formData.append('eval_fdrcapacity', form.eval_fdrcapacity);
+        formData.append('eval_fdrpload', form.eval_fdrpload);
+        formData.append('eval_tilldate', form.eval_tilldate);
+        formData.append('eval_cumloada', form.eval_cumloada);
+
+        formData.append('eval_srcfeeder', form.eval_srcfeeder);
+        formData.append('eval_ptrsf', form.eval_ptrsf);
+        formData.append('eval_trsfrating', form.eval_trsfrating);
+        formData.append('eval_trendpeak', form.eval_trendpeak);
+        formData.append('eval_cumtilldate', form.eval_cumtilldate);
+        formData.append('eval_cummwithload', form.eval_cummwithload);
+        formData.append('eval_permload', form.eval_permload);
+        formData.append('eval_maravail', form.eval_maravail);
+        formData.append('eval_title2', form.eval_title2);
+        formData.append('eval_fulspons', form.eval_fulspons);
+        formData.append('eval_estpcost', form.eval_estpcost);
+        formData.append('eval_specoment', form.eval_specoment);
+        formData.append('eval_preamble', form.eval_preamble);
+        formData.append('eval_findings', form.eval_findings);
+        formData.append('eval_scopework', form.eval_scopework);
+        formData.append('eval_recom', form.eval_recom);
+
+
+        formData.append('eval_titlepro', form.eval_titlepro);
+        formData.append('eval_usercom', form.eval_usercom);
+        formData.append('eval_projmaincat', form.eval_projmaincat);
+        formData.append('eval_dtrating', form.eval_dtrating);
+        formData.append('eval_voltlevel', form.eval_voltlevel);
+        formData.append('eval_subhead', form.eval_subhead);
+        formData.append('eval_voltage_level', form.eval_voltage_level);
+        formData.append('eval_datevisit', form.eval_datevisit);
+        formData.append('eval_specloc', form.eval_specloc);
+        formData.append('eval_majchaexidss', form.eval_majchaexidss);
+        
+        formData.append('eval_nameofsub', form.eval_nameofsub);
+        formData.append('eval_rating', form.eval_rating);
+        formData.append('eval_loading', form.eval_loading);
+        formData.append('eval_loadpercent', form.eval_loadpercent);
+        formData.append('eval_2yrsloadproj', form.eval_2yrsloadproj);
+        formData.append('eval_2yrsloadprojpercent', form.eval_2yrsloadprojpercent);
+        formData.append('eval_amtbillkwh', form.eval_amtbillkwh);
+        formData.append('eval_amtbillnaira', form.eval_amtbillnaira);
+        formData.append('eval_collection', form.eval_collection);
+        formData.append('eval_collectioneff', form.eval_collectioneff);
+
+           
+        formData.append('eval_fdrname2', form.eval_fdrname2);
+        formData.append('eval_fdravail', form.eval_fdravail);
+        formData.append('eval_fdrcapacity2', form.eval_fdrcapacity2);
+        formData.append('eval_fdrtrendpeak', form.eval_fdrtrendpeak);
+        formData.append('eval_fdrdate', form.eval_fdrdate);
+        formData.append('eval_cumload2', form.eval_cumload2);
+        formData.append('eval_srcfeeder2', form.eval_srcfeeder2);
+        formData.append('eval_projcost', form.eval_projcost);
+        formData.append('eval_sanctioncost', form.eval_sanctioncost);
+        formData.append('eval_capcontribproj', form.eval_capcontribproj);
+        formData.append('eval_donor', form.eval_donor);
+        formData.append('eval_ibedc', form.eval_ibedc);
+        formData.append('eval_aprovmbgrant', form.eval_aprovmbgrant);
+        formData.append('eval_recmetertyp', form.eval_recmetertyp);
+        formData.append('eval_statmeter', form.eval_statmeter);
+        formData.append('eval_specoment2', form.eval_specoment2);
+        formData.append('eval_custreq', form.eval_custreq);
+        formData.append('eval_condiag', form.eval_condiag);
+        formData.append('eval_schdiag', form.eval_schdiag);
+        formData.append('eval_sitevform', form.eval_sitevform);
+        formData.append('eval_projplanby', form.eval_projplanby);
+
+
+
+
+
+
+
+
 
 
         formData.append('te_is_connection_approved', 'True');
@@ -631,7 +716,39 @@ addNewUser( formvalue:any
             formData.append('precom_project_title', form.precom_project_title);
             formData.append('precom_last_inspection_date', form.precom_last_inspection_date);
             formData.append('precom_project_objectives', form.precom_project_objectives);
-
+            formData.append('precom_supplysrc', form.precom_supplysrc);
+            formData.append('precom_fdrname3', form.precom_fdrname3);
+            formData.append('precom_peakload', form.precom_peakload);
+            formData.append('precom_dwndrpcon', form.precom_dwndrpcon);
+            formData.append('precom_distofnss', form.precom_distofnss);
+            formData.append('precom_nopoleht', form.precom_nopoleht);
+            formData.append('precom_nopolelt', form.precom_nopolelt);
+            formData.append('precom_podeptht', form.precom_podeptht);
+            formData.append('precom_podepthlh', form.precom_podepthlh);
+            formData.append('precom_sizeconduct', form.precom_sizeconduct);
+            formData.append('precom_qtyused', form.precom_qtyused);
+            formData.append('precom_wellallmetalprt', form.precom_wellallmetalprt);
+            formData.append('precom_ssfencedibedc', form.precom_ssfencedibedc);
+            formData.append('precom_wellgraveled', form.precom_wellgraveled);
+            formData.append('precom_typfence', form.precom_typfence);
+            formData.append('precom_nemsavail', form.precom_nemsavail);
+            formData.append('precom_trsfcap', form.precom_trsfcap);
+            formData.append('precom_voltratio', form.precom_voltratio);
+            formData.append('precom_make', form.precom_make);
+            formData.append('precom_sn', form.precom_sn);
+            formData.append('precom_current', form.precom_current);
+            formData.append('precom_vectorgrp', form.precom_vectorgrp);
+            formData.append('precom_impedance', form.precom_impedance);
+            formData.append('precom_yrsofman', form.precom_yrsofman);
+            formData.append('precom_cooling', form.precom_cooling);
+            formData.append('precom_cabletypsiz', form.precom_cabletypsiz);
+            formData.append('precom_fdrpillarcurr', form.precom_fdrpillarcurr);
+            formData.append('precom_icomcablesiz', form.precom_icomcablesiz);
+            formData.append('precom_uprizercable', form.precom_uprizercable);
+            formData.append('precom_nouprizercable', form.precom_nouprizercable);
+            formData.append('precom_earthresv', form.precom_earthresv);
+            formData.append('precom_pcm', form.precom_pcm);
+            formData.append('precom_others', form.precom_others);
 
 
             formData.append('tept_is_connection_approved', 'True');
